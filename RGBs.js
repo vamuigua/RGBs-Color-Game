a6940991458933a88c31bb00c05993c72e413352
Victor Allen <vamuigua@gmail.com>
2017-09-20 08:06:17 +0300

    Updated functionality for hard mode and easy mode

--- RGBs.js
+++ RGBs.js
@@ -1,25 +1,73 @@
+//Number of colors to generate by default
+var numSquares = 6;
 //Generated Array of random colors
-var colors = generateRandomColors(6);
-
+var colors = generateRandomColors(numSquares);
 //select all squares in the container
 var squares = document.querySelectorAll(".square");
 //pick a random color
 var pickedColor = pickColor();
 //select the colorDisplay element
 var colorDisplay = document.getElementById("colorDisplay");
-//display  the picked color
-colorDisplay.textContent = pickedColor;
 //select the message element
 var messageDisplay = document.querySelector("#message");
 //select the h1 in the document
 var h1 = document.querySelector("h1");
-
 //reset button selector
 var resetButton = document.querySelector("#reset");
+//select easyBtn
+var easyBtn = document.querySelector("#easyBtn");
+//select hardBtn
+var hardBtn = document.querySelector("#hardBtn");
+
+//easyBtn eventlistener
+easyBtn.addEventListener("click", function() {
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
+    //number of squares to display
+    numSquares = 3;
+    //generate 3 new colors
+    colors = generateRandomColors(numSquares);
+    //pick color
+    pickedColor = pickColor();
+    //display picked color
+    colorDisplay.textContent = pickedColor;
+    //give squares new colors
+    for (var i = 0; i < squares.length; i++) {
+        //take all 3 colors generated and display them
+        if (colors[i]) {
+            squares[i].style.background = colors[i];
+        } else {
+            //last 3 squares are not displayed
+            squares[i].style.display = "none";
+        }
+    }
+});
+
+//hardBtn eventlistener
+hardBtn.addEventListener("click", function() {
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+    numSquares = 6;
+    //generate 6 new colors
+    colors = generateRandomColors(numSquares);
+    //pick color
+    pickedColor = pickColor();
+    //display picked color
+    colorDisplay.textContent = pickedColor;
+    //give squares new colors
+    for (var i = 0; i < squares.length; i++) {
+        //take all 3 colors generated and display them
+        squares[i].style.background = colors[i];
+        //let all squares have display of block
+        squares[i].style.display = "block";
+    }
+});
+
+
 //event listener for the resetbutton
 resetButton.addEventListener("click", function() {
     //generate all new colors
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
     //pick new random color from array
     pickedColor = pickColor();
     //change colorDisplay to match picked color
@@ -29,7 +77,7 @@ resetButton.addEventListener("click", function() {
         squares[i].style.background = colors[i];
     }
     //change the h1 background
-    h1.style.background = "#232323"
+    h1.style.background = "#232323";
 })
 
 //for each of the squares, give it a color
